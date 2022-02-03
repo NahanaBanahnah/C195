c6ab93d39491fbebfe407158f0b2cee5123d8e61
Nahana <37905054+NahanaBanahnah@users.noreply.github.com>
2022-02-02 23:37:17 -0600

    Push

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -42,44 +42,91 @@ public class Customer {
         this.cId = cId;
     }
 
+    /** Get ID
+     * Gets Customer ID
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /** Set ID
+     * Sets Customer ID
+     * @param id id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /** Get Name
+     * Gets Customer Name
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /** Get Address
+     * gets address
+     * @return
+     */
     public String getAddress() {
         return address;
     }
 
+    /** Get postal code
+     * gets postal code
+     * @return
+     */
     public String getPostalCode() {
         return postalCode;
     }
 
+    /** Get Phone
+     * gets phone number
+     * @return
+     */
     public String getPhone() {
         return phone;
     }
 
+    /** Get Division
+     * gets Division Name
+     * @return
+     */
     public String getDivision() {
         return division;
     }
 
+    /** Get Country
+     * gets country name
+     * @return
+     */
     public String getCountry() {
         return country;
     }
 
+    /** Get division ID
+     * gets Division ID
+     * @return
+     */
     public int getdId() {
         return dId;
     }
 
+    /** Get Country ID
+     * gets country
+     * @return
+     */
     public int getcId() {
         return cId;
+    }
+
+    /** Overrides to string to change combo-box display
+     * @return country name
+     */
+    @Override public String toString() {
+        return this.name;
     }
 
 }
